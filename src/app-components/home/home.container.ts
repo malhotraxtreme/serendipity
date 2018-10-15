aebd2f38bc3e47d77f358721ee57d23778051daf
Akash Malhotra <akm.2515@gmail.com>
2018-10-14 22:00:53 -0400

    Added icons on home

--- src/app-components/home/home.container.ts
+++ src/app-components/home/home.container.ts
@@ -12,19 +12,25 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
         trigger('slideIn', [
             state('void',style({opacity:0, top:'20px'})),
             transition(':enter',[
-                animate('800ms 0.5s ease')
+                animate('500ms 500ms ease')
             ])
         ]),
         trigger('fade', [
             state('void',style({opacity:0})),
             transition(':enter',[
-                animate('500ms 1s ease')
+                animate('500ms 1000ms ease')
             ])
         ]),
         trigger('slidelater', [
             state('void',style({opacity:0, top:'20px'})),
             transition(':enter',[
-                animate('1200ms 2s ease')
+                animate('500ms 1500ms ease')
+            ])
+        ]),
+        trigger('fadelater', [
+            state('void',style({opacity:0})),
+            transition(':enter',[
+                animate('500ms 2000ms ease')
             ])
         ])
     ]
