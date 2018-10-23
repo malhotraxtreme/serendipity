8f575cfde1b5f3bc9038a2624edc7b8ccf255d72
Akash Malhotra <akm.2515@gmail.com>
2018-10-23 11:59:54 -0400

    Contact page

--- src/app-components/contact/contact.component.ts
+++ src/app-components/contact/contact.component.ts
@@ -1,9 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
+  animations: [
+    trigger('slideIn', [
+        state('void',style({opacity:0, top:'20px'})),
+        transition(':enter',[
+            animate('500ms 500ms ease')
+        ])
+    ]),
+    trigger('fade', [
+        state('void',style({opacity:0})),
+        transition(':enter',[
+            animate('500ms 1000ms ease')
+        ])
+    ]),
+    trigger('slidelater', [
+        state('void',style({opacity:0, top:'20px'})),
+        transition(':enter',[
+            animate('500ms 1500ms ease')
+        ])
+    ]),
+    trigger('fadelater', [
+        state('void',style({opacity:0})),
+        transition(':enter',[
+            animate('500ms 2000ms ease')
+        ])
+    ])
+]
 })
 export class ContactComponent implements OnInit {
 
