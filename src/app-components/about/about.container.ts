8ed7d24daf25c2661080f25288292b86ebec7c25
Akash Malhotra <akm.2515@gmail.com>
2018-10-26 14:51:37 -0400

    fixed some of about page

--- src/app-components/about/about.container.ts
+++ src/app-components/about/about.container.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
     selector: 'about',
     templateUrl: './about.container.html',
-    styleUrls: ['./about.container.css']
+    styleUrls: ['./about.container.css'],
+    animations: [
+        trigger('slideIn', [
+            state('void',style({opacity:0, top:'20px'})),
+            transition(':enter',[
+                animate('500ms 500ms ease')
+            ])
+        ]),
+        trigger('fade', [
+            state('void',style({opacity:0})),
+            transition(':enter',[
+                animate('500ms 1000ms ease')
+            ])
+        ]),
+        trigger('slidelater', [
+            state('void',style({opacity:0, top:'20px'})),
+            transition(':enter',[
+                animate('500ms 1500ms ease')
+            ])
+        ]),
+        trigger('fadelater', [
+            state('void',style({opacity:0})),
+            transition(':enter',[
+                animate('500ms 2000ms ease')
+            ])
+        ])
+    ]
 })
 
 export class AboutContainer{
